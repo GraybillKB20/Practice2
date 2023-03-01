bd563d69f005c94fbb6c2a20149e571f0b495e8a
ZIMMERMANOM22 <zimmermanom22@gcc.edu>
2023-03-01 13:52:21 -0500

    Hello <name> change

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello "+ in.next());
     }
 }
